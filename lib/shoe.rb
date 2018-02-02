14bfa05d8625943d4be66896515eba3f26ba5de6
Eden Mesfun <emesfun@gmail.com>
2018-02-02 17:05:37 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,11 @@ class Shoe
   attr_accessor :color, :material, :condition
   attr_reader :brand, :size
 
-  brands = []
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
-    brands << brand
+    BRANDS << brand
   end
 
   def size=(size)
